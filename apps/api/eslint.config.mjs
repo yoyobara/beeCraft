a1180d0fa9e9ce363e85dc90a88083f5d3d046b6
yoyobara <yotambarak6@gmail.com>
2025-03-15 17:32:21 +0200

    Chore: setup backend and dev script

---
+++ apps/api/eslint.config.mjs
@@ -0,0 +1,11 @@
+import baseConfig from '../../eslint.config.mjs';
+
+export default [
+  ...baseConfig,
+  {
+    files: ['**/*.ts'],
+    // Override or add rules here
+    rules: {},
+  },
+];
+
--- apps/frontend/src/app/App.tsx
+++ apps/frontend/src/app/App.tsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
 export default function App() {
+	const [data, setData] = useState("nodatalol");
+
+	const handleFetch = async () => {
+		const resp = await fetch("http://localhost:3333/api");
+		setData(JSON.stringify(await resp.json()));
+	}
+
 	return (
-		<p>hello</p>
+		<>
+			<button onClick={handleFetch}>fetch!</button>
+			<p>{data}</p>
+		</>
 	)
 }
