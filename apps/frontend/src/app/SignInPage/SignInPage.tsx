821a415a52dc39f7f2df3f0cbc6756be4e38517a
yoyobara <yotambarak6@gmail.com>
2025-03-20 00:20:21 +0200

    unstyled page

--- apps/frontend/src/app/SignInPage/SignInPage.tsx
+++ apps/frontend/src/app/SignInPage/SignInPage.tsx
@@ -1,3 +1,38 @@
+import { useState } from 'react';
+
 export function SignInPage() {
-    return <p>this is sign in page</p>;
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('attempting sign in with', email, password);
+    };
+
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <input
+                        type="email"
+                        required
+                        placeholder="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        required
+                        placeholder="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input type="submit" value="Sign In" />
+                </div>
+            </form>
+        </div>
+    );
 }
