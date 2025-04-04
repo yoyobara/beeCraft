e90a14ebf4cd0db3e81a7d6e8c76d32aa018ed5e
yoyobara <yotambarak6@gmail.com>
2025-04-04 21:03:34 +0300

    better styling

--- apps/frontend/src/app/Navbar/Navbar.tsx
+++ apps/frontend/src/app/Navbar/Navbar.tsx
@@ -2,51 +2,62 @@ import icon from '../../assets/beeNestIcon.png';
 import styles from './Navbar.module.scss';
 import { useAuth } from '../../hooks/auth';
 import { Button } from '../../components/Button';
+import { useNavigate } from 'react-router-dom';
 
 export function Navbar() {
     const { fullName, isLoggedIn, setIsLoggedIn } = useAuth();
+    const navgiate = useNavigate();
 
     const handleLogout = () => {
         setIsLoggedIn(false);
+        navgiate('/');
     };
 
     return (
         <div className={styles.navbar}>
-            <div className={styles.navbar_section}>
-                <img className={styles.icon} src={icon} alt="beeCraft-icon" />
-                <span className={styles.title}>BeeCraft</span>
-                {isLoggedIn && (
-                    <Button variant="primary" kind="contained">
-                        WORLDS
-                    </Button>
-                )}
+            <img className={styles.icon} src={icon} alt="beeCraft-icon" />
+            <span className={styles.title}>BeeCraft</span>
+            {isLoggedIn && (
                 <Button variant="primary" kind="contained">
-                    ABOUT US
+                    WORLDS
                 </Button>
-            </div>
-            <div className={styles.navbar_section}>
-                {isLoggedIn ? (
-                    <>
-                        <span>hello {fullName}</span>
-                        <Button
-                            variant="secondary"
-                            kind="outlined"
-                            onClick={handleLogout}
-                        >
-                            LOG OUT
-                        </Button>
-                    </>
-                ) : (
-                    <>
-                        <Button variant="accent" kind="contained">
-                            SIGN IN
-                        </Button>
-                        <Button variant="accent" kind="outlined">
-                            SIGN UP
-                        </Button>
-                    </>
-                )}
-            </div>
+            )}
+            <Button className={styles.about} variant="primary" kind="contained">
+                ABOUT US
+            </Button>
+            {isLoggedIn ? (
+                <>
+                    <span>hello {fullName}</span>
+                    <Button
+                        variant="secondary"
+                        kind="outlined"
+                        onClick={handleLogout}
+                    >
+                        LOG OUT
+                    </Button>
+                </>
+            ) : (
+                <>
+                    <Button
+                        variant="accent"
+                        kind="contained"
+                        onClick={() => {
+                            navgiate('/sign_in');
+                        }}
+                    >
+                        SIGN IN
+                    </Button>
+                    <Button
+                        variant="accent"
+                        kind="outlined"
+                        onClick={() => {
+                            navgiate('/sign_up');
+                        }}
+                    >
+                        SIGN UP
+                    </Button>
+                </>
+            )}
         </div>
     );
 }
--- apps/frontend/src/components/Button/Button.tsx
+++ apps/frontend/src/components/Button/Button.tsx
@@ -1,16 +1,30 @@
 import clsx from 'clsx';
 import styles from './Button.module.scss';
+import { PropsWithChildren } from 'react';
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends PropsWithChildren {
     variant: 'primary' | 'secondary' | 'accent';
     kind: 'contained' | 'outlined';
+    className?: string;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export function Button({ variant, kind, children, ...props }: ButtonProps) {
+export function Button({
+    variant,
+    kind,
+    className,
+    onClick,
+    children,
+}: ButtonProps) {
     return (
         <button
-            className={clsx(styles.button, styles[variant], styles[kind])}
-            {...props}
+            onClick={onClick}
+            className={clsx(
+                styles.button,
+                styles[variant],
+                styles[kind],
+                className
+            )}
         >
             {children}
         </button>
