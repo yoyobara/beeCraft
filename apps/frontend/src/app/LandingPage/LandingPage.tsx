e7714166223acdf961891990716b8e235ac00575
yoyobara <yotambarak6@gmail.com>
2025-04-16 21:08:10 +0300

    better_bg

--- apps/frontend/src/app/LandingPage/LandingPage.tsx
+++ apps/frontend/src/app/LandingPage/LandingPage.tsx
@@ -1,11 +1,23 @@
+import { Button } from '../../components/Button';
 import styles from './LandingPage.module.scss';
 
 export function LandingPage() {
     return (
-        <>
+        <div className={styles.landing_page}>
             <div className={styles.headline}>
                 Start managing your worlds effortlessly
             </div>
-        </>
+            <div className={styles.subtext}>
+                BeeCraft let's you manage known data about your worlds with ease
+            </div>
+            <div className={styles.buttons}>
+                <Button variant="primary" kind="contained">
+                    SIGN UP
+                </Button>
+                <Button variant="primary" kind="outlined">
+                    SIGN IN
+                </Button>
+            </div>
+        </div>
     );
 }
