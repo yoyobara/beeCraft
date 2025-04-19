d44e3de50287fe774e5567bf6e96bc48d9c037a5
yoyobara <yotambarak6@gmail.com>
2025-04-19 23:27:28 +0300

    fixed navbar and rest heights

--- apps/frontend/src/app/App.tsx
+++ apps/frontend/src/app/App.tsx
@@ -1,13 +1,13 @@
 import '../global.scss';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { PropsWithChildren } from 'react';
+import { useAuth } from '../hooks/auth';
 import styles from './App.module.scss';
 import { Navbar } from './Navbar';
 import { LandingPage } from './LandingPage';
 import { SignInPage } from './SignInPage';
 import { SignUpPage } from './SignUpPage';
 import { WorldsPage } from './WorldsPage';
-import { useAuth } from '../hooks/auth';
-import { PropsWithChildren } from 'react';
 
 export function OnlyGuest({ children }: PropsWithChildren) {
     const { fullName } = useAuth();
@@ -19,33 +19,35 @@ export function App() {
     return (
         <div className={styles.app}>
             <Navbar />
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <OnlyGuest>
-                            <LandingPage />
-                        </OnlyGuest>
-                    }
-                />
-                <Route
-                    path="/sign_in"
-                    element={
-                        <OnlyGuest>
-                            <SignInPage />
-                        </OnlyGuest>
-                    }
-                />
-                <Route
-                    path="/sign_up"
-                    element={
-                        <OnlyGuest>
-                            <SignUpPage />
-                        </OnlyGuest>
-                    }
-                />
-                <Route path="/worlds" element={<WorldsPage />} />
-            </Routes>
+            <div className={styles.rest_of_page}>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <OnlyGuest>
+                                <LandingPage />
+                            </OnlyGuest>
+                        }
+                    />
+                    <Route
+                        path="/sign_in"
+                        element={
+                            <OnlyGuest>
+                                <SignInPage />
+                            </OnlyGuest>
+                        }
+                    />
+                    <Route
+                        path="/sign_up"
+                        element={
+                            <OnlyGuest>
+                                <SignUpPage />
+                            </OnlyGuest>
+                        }
+                    />
+                    <Route path="/worlds" element={<WorldsPage />} />
+                </Routes>
+            </div>
         </div>
     );
 }
