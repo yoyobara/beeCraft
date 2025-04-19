bea27a1cd8a33adb0bbab08d0e2ca10574c1b92d
yoyobara <yotambarak6@gmail.com>
2025-04-19 23:49:10 +0300

    simple world entry in sidebar

--- apps/frontend/src/app/WorldsPage/Sidebar/Sidebar.tsx
+++ apps/frontend/src/app/WorldsPage/Sidebar/Sidebar.tsx
@@ -1,5 +1,14 @@
 import styles from './Sidebar.module.scss';
+import { WorldEntry } from './WorldEntry';
 
 export function Sidebar() {
-    return <div className={styles.sidebar}>this is sidebar</div>;
+    const entries = [1, 2, 3];
+
+    return (
+        <div className={styles.sidebar}>
+            {entries.map((entry) => (
+                <WorldEntry key={entry} />
+            ))}
+        </div>
+    );
 }
--- apps/frontend/src/app/WorldsPage/Sidebar/WorldEntry/WorldEntry.tsx
+++ apps/frontend/src/app/WorldsPage/Sidebar/WorldEntry/WorldEntry.tsx
@@ -1,5 +1,6 @@
+import clsx from 'clsx';
 import styles from './WorldEntry.module.scss';
 
 export function WorldEntry() {
-    return <div>world entry</div>;
+    return <div className={clsx(styles.world_entry)}>world entry</div>;
 }
