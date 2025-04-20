5abd126621de3a996ff13730db59e07470a33a56
yoyobara <yotambarak6@gmail.com>
2025-04-21 00:54:36 +0300

    authenticate middleware

---
+++ apps/api/src/middlewares/auth.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import { User } from '../models';
+
+export async function authenticate(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    if (req.session.loggedInUserId === undefined) {
+        res.status(401).send({ message: 'Unknown user' });
+        return;
+    }
+    const user = await User.findByPk(req.session.loggedInUserId);
+
+    if (user === null) {
+        res.status(401).send({ message: 'Unknown user' });
+        return;
+    }
+
+    req.user = user;
+    next();
+}
