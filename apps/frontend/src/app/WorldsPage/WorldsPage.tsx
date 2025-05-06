7404d063e8fd4e7d71494c94741a4b574d0b24f2
yoyobara <yotambarak6@gmail.com>
2025-05-06 23:52:25 +0300

    removed worlds shit

--- apps/frontend/src/app/WorldsPage/WorldsPage.tsx
+++ apps/frontend/src/app/WorldsPage/WorldsPage.tsx
@@ -1,24 +1,5 @@
-import { useWorlds } from '../../hooks/worlds';
-import styles from './WorldsPage.module.scss';
-import { WorldEntry } from './WorldEntry';
-
 export function WorldsPage() {
-    const { worlds, selectedWorldId } = useWorlds();
 
     return (
-        <div className={styles.worlds_page}>
-            <div className={styles.sidebar}>
-                {worlds.map((world) => (
-                    <WorldEntry
-                        key={world.id}
-                        id={world.id}
-                        name={world.name}
-                    />
-                ))}
-            </div>
-            <div className={styles.mainframe}>
-                this is the mainframe for id {selectedWorldId}
-            </div>
-        </div>
     );
 }
