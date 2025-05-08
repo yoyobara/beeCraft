771938e808038f751cb7b394b6bf5fb10d7caafe
yoyobara <yotambarak6@gmail.com>
2025-05-08 23:16:47 +0300

    very simple style

--- apps/frontend/src/app/WorldsPage/Mainframe/Mainframe.tsx
+++ apps/frontend/src/app/WorldsPage/Mainframe/Mainframe.tsx
@@ -1,6 +1,25 @@
 import clsx from 'clsx';
 import styles from './Mainframe.module.scss';
 
-interface MainframeProps {}
+interface MainframeProps {
+    worldId: number;
+}
 
-export function Mainframe({}: MainframeProps) {}
+export function Mainframe({ worldId }: MainframeProps) {
+    return (
+        <div className={styles.table_container}>
+            <table className={styles.table}>
+                <thead>
+                    <tr className={styles.table_heading}>
+                        <th>Name</th>
+                        <th>X</th>
+                        <th>Y</th>
+                        <th>Z</th>
+                        <th>Notes</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </div>
+    );
+}
