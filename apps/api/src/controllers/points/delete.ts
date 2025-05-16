60f054299f35c5a674caf92083cebbbc66d65081
yoyobara <yotambarak6@gmail.com>
2025-05-16 23:38:12 +0300

    backend deletion

---
+++ apps/api/src/controllers/points/delete.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { PointOfInterest } from '../../models';
+import { msg } from '../../utils/response';
+
+export async function deletePoint(req: Request, res: Response) {
+    const { pointId } = req.body;
+
+    const point = await PointOfInterest.findByPk(pointId);
+    if (!point) {
+        return res.status(404).send(msg('no such point...'));
+    }
+
+    const world = await point.getWorld();
+    if (world?.userId !== req.user.id) {
+        return res
+            .status(403)
+            .send(msg("you don't have access to view this world"));
+    }
+
+    await point.destroy();
+    return res.sendStatus(200);
+}
