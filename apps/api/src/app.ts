1beb72ca2d5f6b2dfd33682ba3e1c3c10e7469ed
yoyobara <yotambarak6@gmail.com>
2025-05-22 00:25:11 +0300

    works in docker compose environment, added proxying

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -6,10 +6,20 @@ import { sessionHandler } from './middlewares/session';
 import { userRouter, worldRouter } from './routes';
 import { pointsRouter } from './routes/points';
 
+console.log('running in', process.env.NODE_ENV);
+
 const app: Express = express();
-const ORIGIN = 'http://localhost:4200';
+const ORIGIN =
+    process.env.NODE_ENV === 'production'
+        ? 'http://localhost/'
+        : 'http://localhost:4200/';
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log('got request');
+    next();
+});
 
 app.use(
     cors({
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './app/App';
 import { AuthProvider } from './hooks/auth';
 
-axios.defaults.baseURL = process.env.NX_PUBLIC_API_URL;
+axios.defaults.baseURL = '/api';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
